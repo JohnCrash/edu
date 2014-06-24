862f3b2c289e9a3e42cb5bac0b030117b2bdd0f0
johncrash <udsn2@sina.com>
2014-06-24 17:56:28 +0800

    增加点cocos 资源。

--- EDEngine/Repository/src/homework/homework.lua
+++ EDEngine/Repository/src/homework/homework.lua
@@ -1,4 +1,4 @@
-local uikits = require "uikits"
+﻿local uikits = require "uikits"
 local kits = require "kits"
 
 print("------------------HomeWork----------------------")
@@ -37,7 +37,13 @@ end
 
 function HomeWork:init()
 	local sv = ccui.ScrollView:create()
-	self._ss = cc.Director:getInstance():getVisibleSize()
+	--local ss = cc.Director:getInstance():getVisibleSize()
+	local director = cc.Director:getInstance()
+	local glview = director:getOpenGLView()
+	local ss = glview:getFrameSize()
+	--print( "getVisibleSize :"..ss.width.." , "..ss.height )
+	InitDesignResolutionMode()
+	self._ss = ss
 	
 	self:addChild(sv)
 	sv:setPosition{x=0,y=0}
@@ -47,10 +53,11 @@ function HomeWork:init()
 	local h = 0
 	for i = 1,32 do
 		--text
-		local t = ccui.Text:create("[ "..i.." ","fonts/Marker Felt.ttf",30)
-		t:setAnchorPoint{x=0.5,y=0}
+		local t = ccui.Text:create("ccui.Text [ "..i.." ]","fonts/Marker Felt.ttf",30)
+		local y =  (i-1)*t:getSize().height
+		t:setAnchorPoint{x=0,y=0}
 		h = h + t:getSize().height
-		t:setPosition{x=self._ss.width/3,y = i*t:getSize().height }
+		t:setPosition{x=0,y = y }
 		sv:addChild(t)
 		--checkbox
 		local c = ccui.CheckBox:create()
@@ -60,10 +67,32 @@ function HomeWork:init()
 								   "cocosui/check_box_active.png",
 								   "cocosui/check_box_normal_disable.png",
 								   "cocosui/check_box_active_disable.png")
-		c:setAnchorPoint{x=0.5,y=0}
-		c:setPosition{x=self._ss.width/1.5,y = i*t:getSize().height }
+		c:setAnchorPoint{x=0,y=0}
+		c:setPosition{x=t:getSize().width,y = y }
 		sv:addChild(c)
-		--image
+		--button
+		local b = ccui.Button:create()
+		b:setScale9Enabled(true)
+		b:loadTextures("cocosui/button.png", "cocosui/buttonHighlighted.png", "")
+		b:setAnchorPoint{x=0,y=0}
+		b:setPosition{x=t:getSize().width+c:getSize().width,y= y }
+		b:setSize{ width = 320,height = c:getSize().height }
+		b:setTitleFontSize( 32 )
+		b:setTitleFontName("fonts/Marker Felt.ttf")
+		b:setTitleText( "ccui.Button 中文"..i )
+		sv:addChild(b)
+		--progress
+		local s = ccui.Slider:create()
+		s:setAnchorPoint{x=0,y=0}
+		s:loadBarTexture("cocosui/sliderTrack.png")
+		s:loadSlidBallTextures("cocosui/sliderThumb.png", "cocosui/sliderThumb.png", "")
+		s:loadProgressBarTexture("cocosui/sliderProgress.png")		
+		s:setPosition{x=b:getPosition()+b:getSize().width,y= y }
+		s:setSize{width=320,height=c:getSize().height }
+		s:setPercent(i*100/32)
+		sv:addChild(s)
+		--edit
+		--local e = ccui.Edit:create()
 	end
 	sv:setInnerContainerSize{width=self._ss.width,height=h}
 end
--- EDEngine/Resources/uikits.lua
+++ EDEngine/Resources/uikits.lua
@@ -9,7 +9,7 @@ function InitDesignResolutionMode()
 	local director = cc.Director:getInstance()
 	local glview = director:getOpenGLView()
 	local ss = glview:getFrameSize()
-	
+	--print( "GLView FrameSize :"..ss.width.." , "..ss.height )
 	director:setContentScaleFactor(1)
-	glview:setDesignResolutionSize(ss.width,ss.height,)
+	glview:setDesignResolutionSize(ss.width,ss.height,cc.ResolutionPolicy.SHOW_ALL)
 end
