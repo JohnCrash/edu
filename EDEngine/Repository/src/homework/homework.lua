96167a677c83336d1b2c6ea5060079be163054b1
johncrash <udsn2@sina.com>
2014-06-25 10:45:42 +0800

    对界面创建进行简单封装

--- EDEngine/Repository/src/homework/homework.lua
+++ EDEngine/Repository/src/homework/homework.lua
@@ -52,44 +52,22 @@ function HomeWork:init()
 	
 	local h = 0
 	for i = 1,32 do
-		--text
-		local t = ccui.Text:create("ccui.Text [ "..i.." ]","fonts/Marker Felt.ttf",30)
+		local t = uikits.text{caption="ccui.Text [ "..i.." ]",fontSize=30}
 		local y =  (i-1)*t:getSize().height
-		t:setAnchorPoint{x=0,y=0}
 		h = h + t:getSize().height
-		t:setPosition{x=0,y = y }
+		t:setPosition{x=0,y=y}
 		sv:addChild(t)
 		--checkbox
-		local c = ccui.CheckBox:create()
-		c:setTouchEnabled(true)
-		c:loadTextures("cocosui/check_box_normal.png",
-								   "cocosui/check_box_normal_press.png",
-								   "cocosui/check_box_active.png",
-								   "cocosui/check_box_normal_disable.png",
-								   "cocosui/check_box_active_disable.png")
-		c:setAnchorPoint{x=0,y=0}
-		c:setPosition{x=t:getSize().width,y = y }
+		local c = uikits.checkbox{x=t:getSize().width,y=y,check=i%2==1 and true or false}
 		sv:addChild(c)
 		--button
-		local b = ccui.Button:create()
-		b:setScale9Enabled(true)
-		b:loadTextures("cocosui/button.png", "cocosui/buttonHighlighted.png", "")
-		b:setAnchorPoint{x=0,y=0}
-		b:setPosition{x=t:getSize().width+c:getSize().width,y= y }
-		b:setSize{ width = 320,height = c:getSize().height }
-		b:setTitleFontSize( 32 )
-		b:setTitleFontName("fonts/Marker Felt.ttf")
-		b:setTitleText( "ccui.Button 中文"..i )
+		local b = uikits.button{x=t:getSize().width+c:getSize().width,y=y,
+											fontSize=32,width=320,height=c:getSize().height,
+											caption="ccui.Button 中文"..i }
 		sv:addChild(b)
-		--progress
-		local s = ccui.Slider:create()
-		s:setAnchorPoint{x=0,y=0}
-		s:loadBarTexture("cocosui/sliderTrack.png")
-		s:loadSlidBallTextures("cocosui/sliderThumb.png", "cocosui/sliderThumb.png", "")
-		s:loadProgressBarTexture("cocosui/sliderProgress.png")		
-		s:setPosition{x=b:getPosition()+b:getSize().width,y= y }
-		s:setSize{width=320,height=c:getSize().height }
-		s:setPercent(i*100/32)
+		--slider
+		local s = uikits.progress{width=320,height=c:getSize().height,
+										x=b:getPosition()+b:getSize().width,y= y,percent=i*100/32}
 		sv:addChild(s)
 		--edit
 		--local e = ccui.Edit:create()
--- EDEngine/Resources/uikits.lua
+++ EDEngine/Resources/uikits.lua
@@ -5,6 +5,9 @@ require "OpenglConstants"
 require "StudioConstants"
 require "GuiConstants"
 
+local defaultFont = "fonts/Marker Felt.ttf"
+local defaultFontSize = 16
+
 function InitDesignResolutionMode()
 	local director = cc.Director:getInstance()
 	local glview = director:getOpenGLView()
@@ -13,3 +16,89 @@ function InitDesignResolutionMode()
 	director:setContentScaleFactor(1)
 	glview:setDesignResolutionSize(ss.width,ss.height,cc.ResolutionPolicy.SHOW_ALL)
 end
+
+local function text( t )
+	local tx
+	if t and type(t)=='table' then
+		tx = ccui.Text:create( t.caption or '',t.font or defaultFont,t.fontSize or defaultFontSize )
+		if tx then
+			tx:setAnchorPoint{x=t.anchorX or 0,y=t.anchorY or 0}
+			tx:setPosition{x=t.x or 0,y=t.y or 0}
+		else
+			print('uikits.text create ccui.Text failed return nil')
+		end
+	end
+	return tx
+end
+
+local function checkbox( t )
+	local cb
+	if t and type(t)=='table' then
+		cb = ccui.CheckBox:create()
+		cb:setTouchEnabled(true)
+		cb:loadTextures(t.normal or "cocosui/check_box_normal.png",
+									t.press or "cocosui/check_box_normal_press.png",
+								   t.active or "cocosui/check_box_active.png",
+								   t.disable or "cocosui/check_box_normal_disable.png",
+								   t.active_disable or "cocosui/check_box_active_disable.png")
+		cb:setAnchorPoint{x=t.anchorX or 0,y=t.anchorY or 0}
+		cb:setPosition{x=t.x or 0,y=t.y or 0}
+		if t.check then
+			cb:setSelectedState( t.check )
+		end
+	end
+	return cb
+end
+
+local function button( t )
+	local cb
+	if t and type(t)=='table' then
+		cb = ccui.Button:create()
+		cb:setScale9Enabled(true)
+		cb:loadTextures(t.normal or "cocosui/button.png", 
+					t.press or "cocosui/buttonHighlighted.png", 
+					t.disable or "")
+		cb:setAnchorPoint{x= t.anchorX or 0,y= t.anchorY or 0}
+		cb:setPosition{x=t.x or 0,y= t.y or 0}
+		cb:setSize{width = t.width or 64,height = t.height or 32}
+		cb:setTitleFontSize( t.fontSize or defaultFontSize )
+		cb:setTitleFontName( t.font or defaultFont)
+		cb:setTitleText( t.caption or '' )
+	end
+	return cb
+end
+
+local function slider( t )
+	local s
+	if t and type(t)=='table' then
+		s = ccui.Slider:create()
+		s:loadBarTexture( t.loadBar or "cocosui/sliderTrack.png")
+		s:loadSlidBallTextures( t.slidBall or "cocosui/sliderThumb.png", "cocosui/sliderThumb.png", "")
+		s:loadProgressBarTexture(t.progressBar or "cocosui/sliderProgress.png")				
+		s:setAnchorPoint{x= t.anchorX or 0,y= t.anchorY or 0}
+		s:setPosition{x=t.x or 0,y= t.y or 0}
+		s:setSize{width=t.width or 160,height=t.height or 32 }
+		s:setPercent( t.percent or 0 )
+	end
+	return s
+end
+
+local function progress( t )
+	local s
+	if t and type(t)=='table' then
+		s = ccui.LoadingBar:create()
+		s:loadTexture(t.progress or "cocosui/sliderProgress.png")
+		s:setAnchorPoint{x= t.anchorX or 0,y= t.anchorY or 0}
+		s:setPosition{x=t.x or 0,y= t.y or 0}		
+		s:setPercent(t.percent or 0)
+	end
+	return s
+end
+
+return {
+	text = text,
+	checkbox = checkbox,
+	button = button,
+	slider = slider,
+	progress = progress
+}
