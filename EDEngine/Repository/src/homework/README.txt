d4c174917b61e0d700212d39e63059a469afcc0d
johncrash <udsn2@sina.com>
2014-07-14 09:45:48 +0800

    框架

---
+++ EDEngine/Repository/src/homework/README.txt
@@ -0,0 +1,8 @@
+﻿homework.lua 入口
+loadingbox.lua 加载，网络错误等公用对话栏
+workloading.lua 加载界面,进入界面
+worklist.lua 作业表
+workflow.lua 作业
+subjective.lua 主观题
+commit.lua 提交
+score.lua 提交结果
---
+++ EDEngine/Repository/src/homework/score.lua
@@ -0,0 +1,57 @@
+﻿local uikits = require "uikits"
+
+local ui = {
+	FILE = 'homework/z3_1/z3_1.json',
+	BACK = 'white/back',
+	HOME = 'Button_31',
+	CAPTION = 'white/lesson_name',
+	END_DATE = 'white/time_over',
+	RANK = 'ranking',
+	TOPICS = 'objective_no',
+	TOPICS2 = 'subjective_no',
+	USE_TIME = 'time',
+	EXP = 'experience_no',
+	SILVER = 'silver_no',
+	GOLD = 'gold_no',
+	LEVEL = 'level',
+	ITEM = 'newview/subject1',
+	GO_WRONG = 'wrong',
+}
+
+local Score = class("Score")
+Score.__index = Score
+
+function Score.create()
+	local scene = cc.Scene:create()
+	local layer = uikits.extend(cc.Layer:create(),Score)
+	
+	scene:addChild(layer)
+	
+	local function onNodeEvent(event)
+		if "enter" == event then
+			layer:init()
+		elseif "exit" == event then
+			layer:release()
+		end
+	end	
+	layer:registerScriptHandler(onNodeEvent)
+	return scene
+end
+
+function Score:init()
+	if not self._root then
+		self._root = uikits.fromJson{file=ui.FILE}
+		local back = uikits.child(self._root,ui.BACK)
+		uikits.event(back,
+			function(sender)
+				uikits.popScene()
+			end)
+		
+	end
+end
+
+function Score:release()
+	
+end
+
+return Score
