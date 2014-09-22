85558860b7a8c11fafce54395ee9b1c5ae808eba
johncrash <udsn2@sina.com>
2014-09-22 15:45:18 +0800

    统计完成

--- EDEngine/Repository/src/homework/worklist.lua
+++ EDEngine/Repository/src/homework/worklist.lua
@@ -271,7 +271,7 @@ function WorkList:init_new_list()
 	self:show_statistics(false)
 	self:show_list(true)
 	self._setting:setVisible(false)
-	
+	self._mode = ui.NEW
 	if not self._scID then -- and not self._busy then
 		self._mode = nil
 		self:clear_all_item()
@@ -310,8 +310,10 @@ function WorkList:init_data_list()
 end
 
 function WorkList:clone_statistics_item(v)
+	self._statistics_item:setVisible(false)
 	local item = self._statistics_item:clone()
 	if item then
+		item:setVisible(true)
 		if v.course and course_icon[v.course] then
 			uikits.child(item,ui.ST_CAPTION):setString(course_icon[v.course].name )
 		end
@@ -415,7 +417,21 @@ function WorkList:date_conv(d)
 		return tostring(d)
 	end
 end
-
+local function minsec(t)
+	local mins = math.floor(t/60)
+	local sec = t - mins*60
+	local result = ''
+	if mins ~= 0 then
+		result = result..mins..'分'
+	end
+	if sec ~= 0 then
+		result = result..sec..'秒'
+	end
+	if result == '' then
+		result = '-'
+	end
+	return result
+end
 function WorkList:statistics_data(t)
 	local idx = {}
 	for i,v in pairs(t) do
@@ -442,17 +458,34 @@ function WorkList:statistics_data(t)
 		{
 			date=self:date_conv(v.year_month),
 			count = v.cnt_home_work,
-			time = v.cnt_times,
+			time = minsec(v.cnt_times),
 			scroe = tostring(score),
 		})
 	end
 	local result = {}
 	local i = 1
 	for k,v in pairs(idx) do
+		if v.pm and type(v.pm)=='table' and #v.pm>0 then
+			if v.t_score and type(v.t_score)=='number' then
+				v.t_score = v.t_score/#v.pm
+			end
+			v.t_times = minsec(v.t_times/#v.pm)
+		end	
 		result[i] = v
 		i = i + 1
 	end
 	return result
+end
+
+function WorkList:clear_statistics()
+	if self._statistics_list then
+		for i ,v in pairs(self._statistics_list) do
+			if v then
+				v:removeFromParent()
+			end
+		end
+		self._statistics_list = {}
+	end
 end
 
 function WorkList:init_statistics()
@@ -462,22 +495,22 @@ function WorkList:init_statistics()
 	self:show_statistics(true)
 	self:show_list(false)
 	self._setting:setVisible(false)
-	if not self._statistics_data then
-		local loadbox = loadingbox.open(self)
-		local url = 'http://new.www.lejiaolexue.com/paper/handler/GetStatisticsStudent.ashx'
-		cache.request_json( url,function(t)
-			if not loadbox:removeFromParent() then
-				return
-			end
-			if t and type(t)=='table' then
-				self._statistics_data = WorkList:statistics_data(t)
-				for i,v in pairs(self._statistics_data) do
-					self:clone_statistics_item(v)
-				end				
-				self:relayout_statistics()
-			end
-		end)
-	end
+	self._mode = ui.STATIST
+	self:clear_statistics()
+	local loadbox = loadingbox.open(self)
+	local url = 'http://new.www.lejiaolexue.com/paper/handler/GetStatisticsStudent.ashx'
+	cache.request_json( url,function(t)
+		if not loadbox:removeFromParent() then
+			return
+		end
+		if t and type(t)=='table' then
+			self._statistics_data = WorkList:statistics_data(t)
+			for i,v in pairs(self._statistics_data) do
+				self:clone_statistics_item(v)
+			end				
+			self:relayout_statistics()
+		end
+	end)
 	return true
 end
 
@@ -488,6 +521,7 @@ function WorkList:init_setting()
 	self:show_statistics(false)
 	self:show_list(false)
 	self._setting:setVisible(true)
+	self._mode = ui.SETTING
 	return true
 end
 
