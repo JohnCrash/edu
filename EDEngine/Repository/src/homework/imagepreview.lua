5e0e91ee4555fc4e8d3b9d2385f1d350373deabd
johncrash <udsn2@sina.com>
2014-11-24 16:17:50 +0800

    image preview

--- EDEngine/Repository/src/homework/imagepreview.lua
+++ EDEngine/Repository/src/homework/imagepreview.lua
@@ -1,3 +1,4 @@
+local kits = require "kits"
 local uikits = require "uikits"
 
 local ui = {
@@ -36,7 +37,6 @@ function ImagePreview:init()
 	self._root = uikits.fromJson{file_9_16=ui.FILE,file_3_4=ui.FILE_3_4}
 	self:addChild(self._root)
 	self._imageview = uikits.child(self._root,ui.IMAGEVIEW)
-	self._scrollview = uikits.child(self._root,ui.SCROLLVIEW)
 	self._pageview = uikits.child(self._root,ui.PAGEVIEW)
 	self._layout = uikits.child(self._root,ui.LAYOUT)
 	self._close = uikits.child(self._root,ui.CLOSE)
@@ -62,7 +62,6 @@ function ImagePreview:init()
 		masize.width = math.max(cs.width,size.width)
 		masize.height = math.max(cs.height,size.height)
 		img:setPosition(cc.p(masize.width/2,masize.height/2))
-		layout._scale = 1	
 	end
 	
 	uikits.event(self._pageview,function(sender,eventType)
@@ -75,24 +74,87 @@ function ImagePreview:init()
 	self._pageview:scrollToPage(self._current-1)
 	
 	--注册滚动事件
+	local newTouch
+	local oldx,oldy,oldscale
 	local function onTouchMoved(touches, event)
-		local count = table.getn(touches)	
-		kits.log("Number of touches: ",count)
-		for i,v in pairs(touches) do
-			kits.log("Touch #"..i)
-			kits.log("getLocation : "..v:getLocation().x..v:getLocation().y)
-			kits.log("getStartLocation : "..v:getStartLocation().x..v:getStartLocation().y)
-			kits.log("getDelta : "..v:getDelta().x..v:getDelta().y)
+		local count = #touches
+		if not newTouch then return end
+		if count == 1 then
+			local idx = self._pageview:getCurPageIndex()
+			local layout = self._pageview:getPage(idx)
+			local img = uikits.child(layout,ui.IMAGEVIEW)
+			local scale = img:getScaleX()
+			local size = img:getContentSize()
+			local p = touches[1]:getLocation()
+			local sp = touches[1]:getStartLocation()			
+			if scale*size.width <=cs.width and scale*size.height <= cs.height then
+				--原图没有缩放,翻页
+				local dx = p.x-sp.x
+				if dx > 100 then
+					local idx = self._pageview:getCurPageIndex()
+					if idx - 1 >= 0 then
+						self._pageview:scrollToPage(idx-1)
+						newTouch = nil
+					end
+				elseif dx < -100 then
+					local idx = self._pageview:getCurPageIndex()
+					local layouts = self._pageview:getPages()
+					if idx + 1 < #layouts then
+						self._pageview:scrollToPage(idx+1)
+						newTouch = nil
+					end			
+				end
+			else
+				--偏移
+				img:setPosition(cc.p(oldx+(p.x-sp.x),oldy+(p.y-sp.y)))
+			end
+		elseif count == 2 then
+			local p1 = touches[1]:getLocation()
+			local sp1 = touches[1]:getStartLocation()
+			local p2 = touches[2]:getLocation()
+			local sp2 = touches[2]:getStartLocation()		
+			local sd = math.sqrt((sp1.x-sp2.x)*(sp1.x-sp2.x) + (sp1.y-sp2.y)*(sp1.y-sp2.y))
+			local d = math.sqrt((p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y))
+			local idx = self._pageview:getCurPageIndex()
+			local layout = self._pageview:getPage(idx)
+			local img = uikits.child(layout,ui.IMAGEVIEW)
+			local scale = d/sd
+			img:setScaleX(scale*oldscale)
+			img:setScaleY(scale*oldscale)
 		end
 	end
 	local function onTouchBegan(touches, event)
+		newTouch = true
+		local idx = self._pageview:getCurPageIndex()
+		local layout = self._pageview:getPage(idx)
+		local img = uikits.child(layout,ui.IMAGEVIEW)
+		oldx,oldy = img:getPosition()
+		oldscale = img:getScaleX()
 		onTouchMoved(touches, event)
 	end	
+	local function onTouchEnded(touches, event)
+		if #touches == 1 then
+			local p = touches[1]:getLocation()
+			local sp = touches[1]:getStartLocation()
+			if math.sqrt((p.x-sp.x)*(p.x-sp.x)+(p.y-sp.y)*(p.y-sp.y)) < 20 then
+				local idx = self._pageview:getCurPageIndex()
+				local layout = self._pageview:getPage(idx)
+				local img = uikits.child(layout,ui.IMAGEVIEW)			
+				img:setScaleX(1)
+				img:setScaleY(1)
+				img:setPosition(cc.p(cs.width/2,cs.height/2))
+			end
+		end
+	end
+	
 	local listener = cc.EventListenerTouchAllAtOnce:create()
 	listener:registerScriptHandler(onTouchBegan,cc.Handler.EVENT_TOUCHES_BEGAN )
 	listener:registerScriptHandler(onTouchMoved,cc.Handler.EVENT_TOUCHES_MOVED )	
-	local eventDispatcher=self._pageview:getEventDispatcher()
-	eventDispatcher:addEventListenerWithSceneGraphPriority(listener,self._pageview)		
+	listener:registerScriptHandler(onTouchEnded,cc.Handler.EVENT_TOUCHES_ENDED )	
+	local eventDispatcher=self:getEventDispatcher()
+	self:setTouchEnabled(true)
+	self._pageview:setTouchEnabled(false)
+	eventDispatcher:addEventListenerWithSceneGraphPriority(listener,self)		
 end
 
 function ImagePreview:release()
