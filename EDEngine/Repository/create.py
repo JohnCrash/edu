23c66e05cbf9ad750e0dd422cea025f6b3b56a28
johncrash <udsn2@sina.com>
2015-03-23 11:43:27 +0800

    开始加入数字对象

--- EDEngine/Repository/create.py
+++ EDEngine/Repository/create.py
@@ -5,21 +5,34 @@ import json
 import codecs
 from datetime import datetime
 
-base = [
-	"7c3064bb858e619b9f02fef85432f162",
-	"b50a67aa2ed2183bee9b804ce7dbdefd",
-	"8bb51443e440190b892996b8c2864672",
-	"8736daf38faaa28693f922843cc0c5aa",
-	"b34d6d7a5652cf3bbe6388d5770dbe95",
-	"6e8c7a6612998e78186585e468010f95",
-	"d55f6d9cbb48b6f402b8122b97ed2dc1",
-	"685316259b01edf58a85d6705a4541ad",	
-	]
+base = {
+	"Root" : "7c3064bb858e619b9f02fef85432f162",
+	"SplashScene" : "b50a67aa2ed2183bee9b804ce7dbdefd",
+	"LoadingScene" : "8bb51443e440190b892996b8c2864672",
+	"Dialog" : "f9e6338b892d9da54fc0668d5f1bd19c",
+	"MessageBox" : "8736daf38faaa28693f922843cc0c5aa",
+	"Spin" : "b34d6d7a5652cf3bbe6388d5770dbe95",
+	"ProgressBox" : "6e8c7a6612998e78186585e468010f95",
+	"Scene" : "d55f6d9cbb48b6f402b8122b97ed2dc1",
+	"Layer" : "685316259b01edf58a85d6705a4541ad",
+	"Widget" : "67f40d28c8cb0b5141b7d8dca355ad58",
+	"Layout" : "d00696dff16c2e217c24cc1fed8ba49e",
+	"Button" : "e051f121c6a2ec01bae324c270f55ee5",
+	"ScrollView" : "a8e99a03bc830f5d2d904be27302d0c2",
+	"Text" : "ba6864653e414016e9f1848ee5986654",
+	"ProgressBar" : "9b954ac14796cf7c24c4a5863c3f5b93",
+	"ScrollBar" : "3257930559cf3e68453bd537e08f63ff",
+	"PopupMenu" : "b5141f9ea3bfc7f45fda60e8f44dde41",
+	"BaiduVoice" : "5b16c70370e6791975778195b421524b",
+	"Game" : "9bb2c0c57a6b6a3763875bbf71b8a0c2",
+	"Sprite" : "5e0c81c61dcebe5b27b6c6c54b5b4414",
+	"Item" : "f84f4bfc387cdc692c07ab1cc16da18d",
+	}
 	
 def create_class(classid,superid,name,desc):
 	if(os.path.isdir('class/'+classid)):
 		print "class "+classid,"is existed"
-	elif(len(superid)==0 or (not os.path.isdir('class/'+superid) and not superid in base)):
+	elif(len(superid)==0 or (not os.path.isdir('class/'+superid) and (not superid in base.keys()) and (not superid in base.values()))):
 		print "superid ",superid,"is not exist"
 	else:
 		os.mkdir('class/'+classid)
@@ -31,10 +44,10 @@ def create_class(classid,superid,name,desc):
 		descFile.write('	"comment":"'+desc+'",\n')
 		descFile.write('	"pedigree":[\n')
 		if superid in base:
-			if superid=="7c3064bb858e619b9f02fef85432f162":
+			if superid=="7c3064bb858e619b9f02fef85432f162" or "Root":
 				pass
 			else:
-				descFile.write('		"7c3064bb858e619b9f02fef85432f162"\n')	
+				descFile.write('		"Root"\n')	
 		else:
 			#read super desc.json file
 			sdescFile = open('class/'+superid+'/desc.json','rb')
