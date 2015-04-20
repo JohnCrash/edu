0f572353a06b89fe2ef7ca42914fc91844bbfd9f
johncrash <udsn2@sina.com>
2015-04-20 09:08:44 +0800

    跟新

--- EDEngine/Repository/class/033b3be83c70075bbe5f98cc9f634a27/levelscene.lua
+++ EDEngine/Repository/class/033b3be83c70075bbe5f98cc9f634a27/levelscene.lua
@@ -8,6 +8,25 @@ return {
 	ccCreate=function(self)
 		self:attach(gl.glNodeCreate())
 		local function visit()
+			if not self._segments then return end
+			local pt = self:ccNode():convertToNodeSpace(cc.p(0,0))
+			for i,v in pairs(self._show) do
+				v.obj:setVisible(false)
+				table.insert(self._segments[v.index].levels[v.level],v.obj)
+				self._show[i] = nil
+			end
+			for i,v in pairs(self._segs) do
+				if v.endx>=pt.x then
+					if v.offset.x > pt.x+self._ss.width then
+						return
+					end
+					local seg = self._segments[v.index]
+					--先取一个最接近于v.repes的值，然后在取剩下的。
+					--比如要求重复23,mipmap是1,2,3,4,8
+					--那么就是先取8,在取8,在取7,但是没有7,就取一个最接近的4,然后是1
+					--seg.maxlevel v.repes
+				end
+			end
 		end
 		self:ccNode():registerScriptDrawHandler(visit)
 		self:loadFromJson("segment.json")		
@@ -23,6 +42,21 @@ return {
 	release=function(self)
 	end,
 	addSegment=function(self,index,re,pt)
+		local oldpt = cc.p(self._segpt.x,self._segpt.y)
+		if pt then
+			self._segpt = pt
+		else
+			self._segpt.x = self._segpt.x + re*self._grain
+		end
+		table.insert(self._segs,{index=index,repes = re,offset=self._segpt,endx=self._segpt.x+re*self._grain}
+		return {x=oldpt.x,y=oldpt.y,width=re*self._grain}
+	end,
+	getSize=function(self)
+		local t = self._segs[#self._segs]
+		if t and t.offset and self._grain then
+			return cc.size(t.offset.x+t.repes*self._grain,self._height)
+		end
+		return cc.size(0,self._height or 0)
 	end,
 	loadFromJson=function(self,file)
 		local t = self:readJson(file)
@@ -30,12 +64,17 @@ return {
 			self._tdata = t
 			self._grain = t.grain
 			self._segments = t.segments
+			self._segs = {}
+			self._show = {}
+			self._height = 0
+			self._segpt = cc.p(0,0)
 			for i,v in pairs(self._segments) do
 				v.scale = v.scale or cc.p(1,1)
 				v.offset = v.offset or cc.p(0,0)
 				if v.mipmap then
-					v.show = {}
-					v.hide = {}
+					v.levels = {}
+					v.maxlevel = 0
+					
 					for k,img in pairs(v.mipmap) do
 						local obj = cc.Sprint:create()
 						obj:setTexture(self:getR(img))
@@ -44,14 +83,15 @@ return {
 						obj:setScale(v.scale)
 						self:addChild(obj)
 						local s = obj:getContentSize()
+						self._height = math.max(self._height,s.height)
 						local level = math.floor(s.width/self._grain)
 						if s.width/self._grain > level + 0.01 or levle==0 then
 							kits.log("WARNING "..self:getClassid().." loadFromJson")
 							kits.log("	image width is not grain Multiple "..tostring(img))
 						end
-						v.hide[level] = v.hide[level] or {}
-						v.show[level] = v.show[level] or {}
-						table.insert(v.hide[level],obj)
+						v.levels[level] = v.levels[level] or {}
+						v.maxlevel = math.max(v.maxlevel,level)
+						table.insert(v.levels[level],obj)
 					end
 				else
 					kits.log("ERROR "..self:getClassid().." loadFromJson")
