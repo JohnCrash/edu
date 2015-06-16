3f05cd096735da7e82d476431620347fcf126b5b
johncrash <udsn2@sina.com>
2015-06-16 16:35:00 +0800

    增加新的内容 a.增加通知 b.增加新的排行方式

---
+++ EDEngine/Repository/src/hitmouse/notice.lua
@@ -0,0 +1,73 @@
+local kits = require "kits"
+local uikits = require "uikits"
+local cache = require "cache"
+local level = require "hitmouse/level"
+local music = require "hitmouse/music"
+local http = require "hitmouse/hitconfig"
+
+local ui = {
+	FILE = 'hitmouse/xiaoxi.json',
+	FILE_3_4 = 'hitmouse/xiaoxi43.json',
+	BACK = 'ding/fan',
+	LIST = 'gun',
+	ITEM = 'x1',
+	ITEM_TITLE = 'w1',
+	ITEM_TIME = 'w2',
+	ITEM_TEXT = 'w3',
+}
+
+local notice = uikits.SceneClass("notice")
+
+function notice:init()
+	if uikits.get_factor() == uikits.FACTOR_9_16 then
+		self._ss = cc.size(1920,1080)
+	else
+		self._ss = cc.size(1440,1080)
+	end
+	uikits.initDR{width=self._ss.width,height=self._ss.height}
+	if not self._root then
+		self._root = uikits.fromJson{file_9_16=ui.FILE,file_3_4=ui.FILE_3_4}
+		self:addChild(self._root)
+		uikits.event(uikits.child(self._root,ui.BACK),function(sender)
+			uikits.popScene()
+		end)		
+		self._scrollview = uikits.scroll(self._root,ui.LIST,ui.ITEM)
+		uikits.enableMouseWheelIFWindows(self._scrollview)
+		self._curPage = 1
+		uikits.event(self._scrollview._scrollview,function(sender,state)
+			if state == ccui.ScrollviewEventType.scrollToBottom then
+				if not self._done_loading and self._tatolPags and self._curPage < self._tatolPags then
+					self._curPage = self._curPage + 1
+					kits.log("continue loading...")
+					self:initNotices(self._curPage)
+					self._done_loading = true
+				end
+			end
+		end)		
+	end
+end
+
+function notice:initNotices(cur)
+	local send_data = {}
+	http.post_data(self._root,'?',send_data,function(t,v)
+		if t and t==200 and v then
+			http.logTable(v)
+			
+			self._scrollview:relayout()
+			if v.v2 then
+				self._tatolPags = self._tatolPags or v.v2
+			else
+				kits.log("ERROR tops:initTops road_block_rank v.v2 = nil")
+			end
+		else
+			http.messagebox(self._root,http.NETWORK_ERROR,function(e)
+			end)				
+		end
+	end)
+end
+
+function notice:release()
+	uikits.enableMouseWheelIFWindows(nil)
+end
+
+return notice
