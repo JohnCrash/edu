352946c6fc8f26fee3c069f755ee31bc0231654a
john <udsn2@sina.com>
2016-11-30 09:45:56 +0800

    add preview

--- EDEngine/Classes/live/ffdec.cpp
+++ EDEngine/Classes/live/ffdec.cpp
@@ -230,6 +230,13 @@ namespace ff
 		PUT_TPS_BY_VALUE(praw, TP_SWS1, pt2);
 		PUT_TPS(praw, TP_READFRAME_RETURN);
 	}
+	static void AddPreview(AVDecodeCtx *pdc,AVRaw *raw){
+		if (pdc->preview_thread){
+			mutex_lock_t lock(*pdc->preview_mutex);
+			pdc->perview_frames->push_front(raw_ref(raw));
+			pdc->preview_cond->notify_one();
+		}
+	}
 	/*
 	 * 从文件或者设备中解码帧数据 
 	 */
@@ -295,6 +302,7 @@ namespace ff
 					av_frame_unref(frame);
 					
 					put_tp(praw, pt0, pt1, pt2);
+					AddPreview(pdc,praw);
 					return praw;
 				}
 				else if (ret == AVERROR_EOF || ret == AVERROR(EINVAL)){
